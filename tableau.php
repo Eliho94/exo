1376f9a0de6ba1472f934479e5482ab7862e59af
Rakotoarisoa <eliho.rakotoarisoa@gmail.com>
2023-11-09 09:27:56 +0100

    change keys

--- tableau.php
+++ tableau.php
@@ -1,12 +1,12 @@
 
 <?php
 $movies = [
-    1981 => "Les Aventuriers de l'arche perdue",
-    2008 => "Indiana Jones et le royaume du crâne de cristal",
-    1988 => "Piège de cristal",
+    "Les Aventuriers de l'arche perdue" => 1981  ,
+    "Indiana Jones et le royaume du crâne de cristal" => 2008  ,
+   "Piège de cristal"  =>  1988,
 ];
-ksort($movies);
+arsort($movies);
 foreach($movies as $key => $movie){
-    echo $key ."-". $movie ; 
+    echo $key ."-". $movie . PHP_EOL ; 
      
 }
